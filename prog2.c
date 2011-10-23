2b01afe5a5de2d7eafc793f9b937fcdb66e58115
Lukas <lsady@sigma.ug.edu.pl>
2011-10-23 10:01:43 +0200

    prog2.c

---
+++ prog2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float n1,n2,n3;
+
+int main() {
+  n1=5+3*8/2-3;
+  n2=17%3*11;
+  n2=2*4*(5+9.0/2);
+  printf("n1 = %4.0f\nn2 = %4.0f\nn3 = %4.0f\n\n" ,\ 
+	 n1,n2,n3);
+  return 0;
+}
